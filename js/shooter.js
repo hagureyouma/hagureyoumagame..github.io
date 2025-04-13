0e9a7ba48f98fe40d7587a9eb5c140d89cfd6be8
Mogege Boku <bokumogege@hotmail.co.jp>
2025-04-13 09:06:35 +0900

    ああ

--- js/shooter.js
+++ js/shooter.js
@@ -24,6 +24,7 @@
 }
 'use strict';
 console.clear();
+//ああああ
 
 import { cfg, EMOJI, game, Util, Mono, Coro, wait, waitForFrag, waitForTime, waitForTimeOrFrag, Child, Pos, Scale, Move, Anime, Ease, Guided, Collision, Brush, Tofu, Moji, Label, Particle, Gauge, OutOfRangeToRemove, OutOfScreenToRemove, Menu, Watch, Color, through } from "./youma.js";
 
